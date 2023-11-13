0e11627e666cad2a701d8f511bf29483782de876
Nathan Hoang <sliqq123@gmail.com>
2023-11-12 16:41:03 -0800

    added detect language function

--- translate.js
+++ translate.js
@@ -1,39 +1,24 @@
-const ogLyrics = document.getElementById('oglyrics')
-const translatedLyrics = document.getElementById('translatedLyrics')
-const language = document.getElementById('language')
-const translateBtn = document.getElementById('translate')
+const ogLyrics = document.getElementById('oglyrics');
+const translatedLyrics = document.getElementById('translatedLyrics');
+const language = document.getElementById('language');
+const translateBtn = document.getElementById('translate');
 
-translateBtn.addEventListener('click', async () => {
-    const lyricsUntranslated = ogLyrics.value;
-    const translatelanguage = language.value;
+// Function to detect language
+async function detectLanguage(text) {
+    const detectUrl = 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect';
+    const detectOptions = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Accept-Encoding': 'application/gzip',
+            'X-RapidAPI-Key': 'YOUR_API_KEY', // Replace with your API key
+            'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+        },
+        body: new URLSearchParams({ q: text })
+    };
 
-
-
-    
-    
-    const url = 'https://google-translate1.p.rapidapi.com/language/translate/v2';
-    const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'Accept-Encoding': 'application/gzip',
-		'X-RapidAPI-Key': '1eb102c1ccmsh8787a73e30281e3p11f25ajsncb90c9ddb7c3',
-		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-	},
-	body: new URLSearchParams({
-		q: lyricsUntranslated,
-		target: translatelanguage,
-		source: 'en'
-	})
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+    const response = await fetch(detectUrl, detectOptions);
+    const data = await response.json();
+    return data.data.detections[0][0].language; // Assuming the API response has this structure
 }
 
-
-})
